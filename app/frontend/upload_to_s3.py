1b84a8a04ef5cb6fceb11bfbedd701f58d888901
My Name <My_email>
2025-10-26 04:24:13 -0700

    "updated ui, implemented backend with the model"

--- app/frontend/upload_to_s3.py
+++ app/frontend/upload_to_s3.py
@@ -1,11 +1,16 @@
 import boto3
 import os
 
-# --- AWS CONFIG ---
-AWS_ACCESS_KEY_ID = "AKIA5EOQSV5VI4IEKW4X"
-AWS_SECRET_ACCESS_KEY = "Yj3dRu+dfVN9xKPT8t/d4WUP1L4apLdZR1lMQhzh"
-AWS_REGION = "us-east-1"
-S3_BUCKET = "lung-xray-uploads"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Put your AWS credentials in a .env file
+access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+bucket_name = os.getenv("S3_BUCKET")
+aws_region = os.getenv("AWS_REGION")
+
 # ------------------
 
 def upload_file_to_s3(local_path, filename):
@@ -13,16 +18,21 @@ def upload_file_to_s3(local_path, filename):
         # Initialize S3 client
         s3 = boto3.client(
             's3',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            region_name=AWS_REGION
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            region_name=aws_region
         )
 
+
+        print("DEBUG values:")
+        print("local_path =", type(local_path), local_path)
+        print("bucket_name =", bucket_name)
+        print("filename =", filename)
         # Upload the file
-        s3.upload_file(local_path, S3_BUCKET, filename)
+        s3.upload_file(local_path, bucket_name, filename)
 
         # Construct a public URL (if your bucket allows public access)
-        public_url = f"https://{S3_BUCKET}.s3.{AWS_REGION}.amazonaws.com/{filename}"
+        public_url = f"https://{bucket_name}.s3.{aws_region}.amazonaws.com/{filename}"
 
         print(f"✅ Upload successful! File available at: {public_url}")
         return True, public_url
--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_file_view, name='home'),          # 👈 This handles http://127.0.0.1:8000/
-    path('upload/', views.upload_file_view, name='upload_file'),
+    path("", views.index, name="index"),
+    path("upload/", views.upload_file_view, name="upload_file"),
+    path("results/", views.results_view, name="results"),
 ]
